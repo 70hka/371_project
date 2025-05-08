7a5ec82456fc343d39dcdc73f91edf78e9cfed81
Jamal Mohamed <jzachmoh@gmail.com>
2025-05-07 20:56:39 -0500

    Added connection file for the database

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "rei.cs.ndsu.nodak.edu";
+$username = "jamal_mohamed_371s25";
+$password = "WLdT3N2aMN0!";
+$database = "jamal_mohamed_db371s25";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+echo "Connected successfully";
+?>
