9d81b5646fa0a990428c93b8747c00ed96f71c94
Jamal Mohamed <jzachmoh@gmail.com>
2025-05-08 04:36:46 -0500

    Made redirections for auth.php go to respective page if cookie exists

--- auth.php
+++ auth.php
@@ -2,6 +2,7 @@
 session_start();
 
 function require_login($required_role = null) {
+    // Set session from cookies if needed
     if (!isset($_SESSION['user_id']) && isset($_COOKIE['user_id'])) {
         $_SESSION['user_id'] = $_COOKIE['user_id'];
         $_SESSION['username'] = $_COOKIE['username'];
@@ -9,13 +10,21 @@ function require_login($required_role = null) {
         $_SESSION['email'] = $_COOKIE['email'];
     }
 
+    // If no user ID send to login
     if (!isset($_SESSION['user_id'])) {
         header("Location: login.php");
         exit;
     }
 
+    // If role is required and doesn't match, redirect to proper dashboard
     if ($required_role !== null && $_SESSION['role'] !== $required_role) {
-        header("Location: login.php");
+        if ($_SESSION['role'] === 'student') {
+            header("Location: studentdash.php");
+        } elseif ($_SESSION['role'] === 'instructor') {
+            header("Location: instructordash.php");
+        } else {
+            header("Location: login.php"); // fallback condition
+        }
         exit;
     }
 }
